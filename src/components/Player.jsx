9e821199f27ad2effb73bba2fea3243261e25024
Bruno Gabriel Santos <brunogbl1sts@gmail.com>
2024-11-12 14:37:14 -0300

    Inclusão de texto

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { songsData } from '../assets/assets'
+import { assets, songsData } from '../assets/assets'
 
 const Player = () => {
   return (
@@ -11,7 +11,15 @@ const Player = () => {
           <p>{songsData[0].desc.slice(0,12)}</p>
         </div>
       </div>
-      <div className='flex flex-col'>
+      <div className='flex flex-col items-center gap-1 m-auto'>
+        <div className='flex gap-4'>
+          <img className='w-4 cursor-pointer' src={assets.shuffle_icon} alt="" />
+          <img className='w-4 cursor-pointer' src={assets.prev_icon} alt="" />
+          <img className='w-4 cursor-pointer' src={assets.play_icon} alt="" />
+          <img className='w-4 cursor-pointer' src={assets.next_icon} alt="" />
+          <img className='w-4 cursor-pointer' src={assets.loop_icon} alt="" />
+
+        </div>
 
       </div>
     </div>
