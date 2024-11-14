ab5607d544c5cb41395c8561500075496a4726c1
Bruno Gabriel Santos <brunogbl1sts@gmail.com>
2024-11-14 17:16:31 -0300

    Inclusão de texto

--- src/components/DisplayAlbum.jsx
+++ src/components/DisplayAlbum.jsx
@@ -1,17 +1,26 @@
 import React from 'react'
 import Navbar from './Navbar'
 import { useParams } from 'react-router-dom'
+import { albumsData } from '../assets/assets';
 
 const DisplayAlbum = () => {
-
-  const DisplayAlbum = () => {
     
-    const {id} = useParams()
-  }
-
+    const {id} = useParams();
+    const albumData = albumsData[id]
+  
   return (
     <>
       <Navbar/>
+      <div className='mt-10 flex gap-8 flex-col md:flex-row md:items-end'>
+        <img className='w-48 rounded' src={albumData.image} alt="" />
+        <div className='flex flex-col'>
+          <p>Playlist</p>
+          <h2 className='text-5xl font-bold mb-4 md:text-7x1'>{albumData.name}</h2>
+          <h4></h4>
+
+        </div>
+
+      </div>
     </>
   )
 }
