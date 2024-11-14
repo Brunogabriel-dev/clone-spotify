db440b00b9f8308225d0a4bbcc211683a29f522f
Bruno Gabriel Santos <brunogbl1sts@gmail.com>
2024-11-14 16:52:58 -0300

    Inclusão de texto

---
+++ src/components/DisplayAlbum.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import Navbar from './Navbar'
+import { useParams } from 'react-router-dom'
+
+const DisplayAlbum = () => {
+
+  const DisplayAlbum = () => {
+    
+    const {id} = useParams()
+  }
+
+  return (
+    <>
+      <Navbar/>
+    </>
+  )
+}
+
+export default DisplayAlbum
